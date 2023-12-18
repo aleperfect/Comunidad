0ffb6cf4455d7782ce39a40480be3ec3e370dd9a
aleperfect <143966062+aleperfect@users.noreply.github.com>
2023-12-18 08:34:55 -0500

    Aleizn

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,17 +4,17 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `¿ ‼️Deseas un bot para tu grupo comunicame aqui‼️ ? Wa.me/51992621601
+let oi = `*ANOTATE CRIATURA DEL SEÑOR SINO CUPO:* ${pesan}`
+let teks = `¿ ‼️Deseas un bot para tu grupo‼️ ? Wa.me/56940831045
 *⺀👻Madrugar se hace más fácil contigo a mi lado⺀*\n\n🟩 ${oi}\n\n🟩 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┊🐲 @${mem.id.split('@')[0]}\n`}
-teks += `👤𝐄𝐯𝐞𝐧𝐭𝐨𝐬 𝐩𝐚𝐠𝐚 𝟏𝟑`
+teks += `⚡@𝐄𝐕𝐄𝐍𝐓𝐎𝐒_𝐏𝐀𝐆𝐀_𝟏𝟑`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten|eventos13|adan13|ep13)$/i
 handler.admin = true
 handler.group = true
 export default handler
